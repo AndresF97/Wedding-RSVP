6b1a095a37c21b845de82950666a0c7c13ae8620
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2022-09-26 14:59:45 +0900

    set up relationship must test to see if its correct

--- models/index.js
+++ models/index.js
@@ -2,8 +2,24 @@ const User = require('./User')
 const Wedding = require('./Wedding')
 const Guest = require('./Guest')
 const Dish = require('./Dish')
+// Set up realtionship next
 
 
+Wedding.belongsTo(User,{
+    foreign_key:'user_id',
+    onDelete:"CASACADE"
+})
+
+Guest.belongsTo(Wedding,{
+    foreign_key:'guest_list',
+    onDelete:'CASCADE'
+})
+Wedding.hasMany(Guest,{
+    foreign_key:'guest_list'
+})
+Dish.belongsTo(Wedding,{
+    foreign_key:'dish_id'
+})
 
 module.exports = {
     User, Wedding, Guest,
