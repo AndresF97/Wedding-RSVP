72c9475e9e479075c21bb7e5a1d4dfa3455fc233
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2022-10-04 18:28:56 +0900

    set up realtionship, will test it out later in the routes branch

--- models/index.js
+++ models/index.js
@@ -3,39 +3,26 @@ const Wedding = require('./Wedding')
 const Guest = require('./Guest')
 const Dish = require('./Dish')
 // Set up realtionship next
-// User.belongsTo(Wedding,{
-//     foreignKey:'user_id',
-//     onDelete:'CASCADE'
-// })
-// User.belongsTo(Guest,{
-//     foreignKey:'user_id',
-// })
+Wedding.belongsTo(User,{
+    foreignKey:'user_id',
+    onDelete:'CASCADE'
+})
 
-// Wedding.hasMany(User,{
-//     foreignKey:'user_id',
-//     onDelete:"CASACADE"
-// })
-// Wedding.hasMany(Guest,{
-//     foreignKey:'guest_list',
-// })
 
-// Guest.hasMany(Wedding,{
-//     foreignKey:'guest_list',
-//     onDelete:'CASCADE'
-// })
-// Guest.belongsTo(Wedding,{
-//     foreignKey:'wedding_event',
-//     onDelete:'CASCADE'
-// })
-// User.belongsTo(Guest,{
-//     foreignKey:'user_id',
-//     onDelete:"CASACADE"
-// })
+Wedding.hasMany(User,{
+    foreignKey:'user_id',
+    onDelete:"CASACADE"
+})
 
-// Dish.belongsTo(Wedding,{
-//     foreignKey:'dish_id',
-//     onDelete:"CASACADE"
-// })
+Wedding.hasMany(Guest,{
+    foreignKey:'wedding_id',
+    onDelete:'CASCADE'
+})
+
+Dish.belongsTo(Wedding,{
+    foreignKey:'dish_id',
+    onDelete:"CASACADE"
+})
 
 module.exports = {
     User, Wedding, Guest,
